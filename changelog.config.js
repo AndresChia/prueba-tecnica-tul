ddc86fbf666ffc701fc68678a7e402cf2d8b39b0
Andres_Chia <achia@soytul.com>
2022-08-08 11:48:46 -0500

    feat: 🎸 code structure update

---
+++ changelog.config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  disableEmoji: false,
+  format: '{type}{scope}: {emoji}{subject}',
+  list: ['test', 'feat', 'fix', 'chore', 'merge', 'docs', 'refactor', 'style', 'ci', 'build', 'perf'],
+  maxMessageLength: 64,
+  minMessageLength: 3,
+  questions: ['type', 'scope', 'subject', 'body', 'breaking', 'issues', 'lerna'],
+  scopes: [],
+  types: {
+    merge: {
+      description: 'Merge change',
+      emoji: '☝',
+      value: 'merge'
+    },
+    chore: {
+      description: 'Build process or auxiliary tool changes',
+      emoji: '🤖',
+      value: 'chore'
+    },
+    ci: {
+      description: 'CI related changes',
+      emoji: '🎡',
+      value: 'ci'
+    },
+    build: {
+      description: 'Add or update regards to build process',
+      emoji: '🏗️',
+      value: 'build'
+    },
+    docs: {
+      description: 'Documentation only changes',
+      emoji: '✏️',
+      value: 'docs'
+    },
+    feat: {
+      description: 'A new feature',
+      emoji: '🎸',
+      value: 'feat'
+    },
+    fix: {
+      description: 'A bug fix',
+      emoji: '🐛',
+      value: 'fix'
+    },
+    perf: {
+      description: 'A code change that improves performance',
+      emoji: '⚡️',
+      value: 'perf'
+    },
+    refactor: {
+      description: 'A code change that neither fixes a bug or adds a feature',
+      emoji: '💡',
+      value: 'refactor'
+    },
+    style: {
+      description: 'Markup, white-space, formatting, linter, missing semi-colons...',
+      emoji: '💄',
+      value: 'style'
+    },
+    test: {
+      description: 'Adding missing tests',
+      emoji: '💍',
+      value: 'test'
+    }
+  }
+};
